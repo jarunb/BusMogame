28c623eb7ebcf8105c8ab95d62fc2e3cad897333
jarunb <134996296+jarunb@users.noreply.github.com>
2023-05-30 13:03:46 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1685423398,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bmgame-sheet0.png",
+		"images/jigzaw-sheet0.png",
+		"images/pic-sheet0.png",
+		"images/canvas.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
